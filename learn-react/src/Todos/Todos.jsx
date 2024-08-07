144b535e75bbd7826198c9c5b334e61f75f33dca
Shrikant Yadav <shrikant9907@gmail.com>
2024-08-07 22:44:55 +0530

    Todo List with localstorage

--- learn-react/src/Todos/Todos.jsx
+++ learn-react/src/Todos/Todos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodosHeader from './TodosHeader'
 import TodosFooter from './TodosFooter'
 import TodosForm from './TodosForm'
@@ -7,8 +7,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Todos = () => {
 
-    const initialTask = { title: "", id: uuidv4() }
+    const initialTask = { id: uuidv4(), title: "" , status: 'Todo' }
     const [task, setTask] = useState(initialTask)
+    const [editTask, setEditTask] = useState(false)
     const [todoList, setTodoList] = useState([]);
 
     const handleOnTaskChange = (e) => {
@@ -18,9 +19,64 @@ const Todos = () => {
 
     const handleOnSubmit = (e) => {
         e.preventDefault();
-
-        setTodoList([...todoList, task])
+        const newTodoList = [...todoList, task];
+        setTodoList(newTodoList)
+        localStorage.setItem('todoList', JSON.stringify(newTodoList))
         setTask(initialTask) // Resetting the value of task
+        if (editTask) {
+
+          // Shallow Copy
+          const filteredItems = todoList.slice();
+
+          // Finding the index of the object / item / task
+          const indexOfEdit = todoList.findIndex(item => item.id === task.id);
+          
+          // Replacing the item
+          filteredItems[indexOfEdit] = task;
+
+          // Updating this inside thestate and the localstorage.
+          setTodoList(filteredItems)
+          localStorage.setItem('todoList', JSON.stringify(filteredItems))
+
+          // Updating the edit state
+          setEditTask(false);
+          
+        }
+    }
+
+    useEffect(() => {
+      const todoListLS = localStorage.getItem('todoList');
+      if (todoListLS) {
+        const todoListLSArr = JSON.parse(todoListLS)
+        setTodoList(todoListLSArr)
+      }
+    }, [])
+
+
+    const handleOnDelete = (item) => {
+      const filteredItems = todoList.filter((task) => task.id !== item.id);
+      setTodoList(filteredItems)
+      localStorage.setItem('todoList', JSON.stringify(filteredItems))
+    }
+
+    const handleOnEdit = (item) => {
+      setEditTask(true)
+      setTask(item);
+    }
+
+    const handleOnComplete = (task) => {
+      // Shallow Copy
+      const filteredItems = todoList.slice();
+
+      // Finding the index of the object / item / task
+      const indexOfEdit = todoList.findIndex(item => item.id === task.id);
+      
+      // Replacing the item
+      filteredItems[indexOfEdit] = {...task, status: "Completed"};
+
+      // Updating this inside thestate and the localstorage.
+      setTodoList(filteredItems)
+      localStorage.setItem('todoList', JSON.stringify(filteredItems))
     }
 
     console.log('test_debug', todoList)
@@ -28,8 +84,20 @@ const Todos = () => {
   return (
     <div>
         <TodosHeader />
-        <TodosForm onSubmit={handleOnSubmit} task={task} onChange={handleOnTaskChange} />
-        <TodosList list={todoList} />
+        
+        <TodosForm 
+          onSubmit={handleOnSubmit} 
+          task={task} 
+          onChange={handleOnTaskChange}
+        />
+
+        <TodosList 
+          list={todoList} 
+          onDelete={handleOnDelete} 
+          onEdit={handleOnEdit} 
+          onComplete={handleOnComplete}  
+        />
+        
         <TodosFooter />
     </div>
   )
--- learn-react/src/Todos/TodosList.jsx
+++ learn-react/src/Todos/TodosList.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const TodosList = ({list}) => {
+const TodosList = ({list, onDelete, onEdit, onComplete}) => {
   return (
     <div className='px-4'>
         <h3 className='mb-2 font-bold'>Task List:</h3>
         <ul>
           {list && list.map((item) => {
-            return <li className='border-t p-2'>{item.title}</li> 
+            return <li className='border-t p-2 flex justify-between gap-3'>
+             <div>
+             <strong>{item.title}</strong> <br />
+             <em className='border px-2 bg-blue-50 rounded-md'>{item.status}</em>
+             </div>
+              <div className='flex gap-3 justify-end'>
+                <button onClick={() => onComplete(item)} className='bg-blue-500 p-2 py-1 text-white rounded-lg'>Mask As Completed</button>
+                <button onClick={() => onEdit(item)} className='bg-blue-500 p-2 py-1 text-white rounded-lg'>Edit</button>
+                <button onClick={() => onDelete(item)} className='bg-red-500 p-2 py-1 text-white rounded-lg'>Delete</button>
+              </div>
+            </li> 
           })}
           
         </ul>
